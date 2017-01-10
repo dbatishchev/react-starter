4e187ff19926da0290d8f64ff1451d25b087629c
dbatishchev <dimitrybatishchev@gmail.com>
2017-01-10 21:50:14 +0300

    remove extra deps

--- src/store.js
+++ src/store.js
@@ -3,17 +3,12 @@ import { browserHistory } from "react-router";
 import { syncHistoryWithStore, routerMiddleware } from "react-router-redux";
 import createSagaMiddleware from "redux-saga";
 import { reducers } from "./reducers/index";
-import { sagas } from "./sagas/index";
 
 // add the middlewares
 let middlewares = [];
 
 // add the router middleware
 middlewares.push(routerMiddleware(browserHistory));
-
-// add the saga middleware
-const sagaMiddleware = createSagaMiddleware();
-middlewares.push(sagaMiddleware);
 
 // // add the freeze dev middleware
 // if (process.env.NODE_ENV !== 'production') {
@@ -31,7 +26,6 @@ if (process.env.NODE_ENV !== 'production' && window.devToolsExtension) {
 // create the store
 const store = createStore(reducers, middleware);
 const history = syncHistoryWithStore(browserHistory, store);
-sagaMiddleware.run(sagas);
 
 // export
 export { store, history };
